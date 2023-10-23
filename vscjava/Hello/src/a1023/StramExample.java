71e6ef247984afb3500ab797bdb8b37596be918b
kangkyengsin <shin295@naver.com>
2023-10-23 12:55:33 +0900

    1023

---
+++ vscjava/Hello/src/a1023/StramExample.java
@@ -0,0 +1,27 @@
+package a1023;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class StramExample {
+    public static void main(String[] args) {
+        // Set 컬렉션 생성
+        Set<String> set = new HashSet<>();
+        set.add("홍길동");
+        set.add("신용권");
+        set.add("감자바");
+
+        // Strean을 이용한 요소 반복 처리
+        Stream<String> stream = set.stream();
+        stream.forEach(name -> System.out.println(name));
+        
+        // Iterator를 이용한 요소 반복 처리
+        // Iterator<String> iterator = set.iterator();
+        // while (iterator.hasNext()) {
+        //     String name = iterator.next();
+        //     System.out.println(name);
+        // }
+    }
+}
---
+++ vscjava/Hello/src/a1023/ch17/sec07/exam02/Student.java
@@ -0,0 +1,14 @@
+package a1023.ch17.sec07.exam02;
+
+public class Student {
+	private String name;
+	private int score;
+
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	public String getName() { return name; }
+	public int getScore() { return score; }
+}
