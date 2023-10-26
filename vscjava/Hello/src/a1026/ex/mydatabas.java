9d92bf82ac877ec497fe49f075d222bb7d76f2ac
kangkyengsin <shin295@naver.com>
2023-10-26 17:39:39 +0900

    1026

--- vscjava/Hello/src/a1026/ex/mydatabas.java
+++ vscjava/Hello/src/a1026/ex/mydatabas.java
@@ -3,14 +3,16 @@ package a1026.ex;
 public class mydatabas {
     CREATE DATABASE mydatabas
 
+    show databases // 처음 데이터베이스 확인
+
     USE mydatabas
     
-    CREATE TABLE students(
+    CREATE TABLE students( // 테이블 추가
         id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(50),
         math_score INT,
         science_score INT,
-        english_score INT,
+        english_score INT
     );
     
     INSERT INTO students(name, math_score, science_score, english_score) VALUES ('John Doe', 90, 85, 78),('Jane Smith', 88, 92, 76),('Bob Johnson', 76, 89, 82);
