fea94087984636afba270055abd7238b402ad8fa
kangkyengsin <shin295@naver.com>
2023-12-07 11:15:56 +0900

    1207

--- react/react-photo/src/Child.js
+++
@@ -1,5 +0,0 @@
-function Child() {
-    return <h3>나는 자식입니다.</h3>;
-   }
-   
-export default Child;
--- react/react-photo/src/components/MemberItemLIst.js
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-import ItemThumb from './ItemThumb';
-import ItemDesc from './ItemDesc';
-const MemberItemList = (props) => {
-
-};
-export default MemberItemList;
